741dc34d58a640cf97103e3ab7ae45d64203bac2
Kyle Lux <kyleslux86@gmail.com>
2021-11-02 08:32:10 -0400

    Update server.js
    Removed helper reference

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection.js");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const hbs = exphbs.create({helpers});
 
 const sess = {
     secret: "Super secret secret",
