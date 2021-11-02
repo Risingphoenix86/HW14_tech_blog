cee05907096d6d51cbf200d41dbd9ceb9f8b1346
Kyle Lux <kyleslux86@gmail.com>
2021-11-02 08:36:35 -0400

    Update server.js
    fix typo in date_format function

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const sess = {
 const hbs = express.create({
     helpers: {
         format_date: date => {
-            return 1${date.getMonth() +1}/${date.getDate()}/${date.getFullYear()}`;
+            return `${date.getMonth() +1}/${date.getDate()}/${date.getFullYear()}`;
         }
     }
 });
